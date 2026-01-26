f97c3893e6d437d4540c5c80601c797e96da957e
SATYAM MAURYA <satyammaurya24363@gmail.com>
2026-01-26 08:10:53 +0530

    day2

---
+++ day2/basic.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // cout << "Namste Duniya" << endl;
+    // cout << "Hello,Satyam";
+    // cout << endl;
+    // int a = 5;
+    // char ch = 'A';
+    // bool b1 = true;
+    // float c = 1.23;
+    // double d = 43.87833;
+    // cout << a << " Size of a is=" << sizeof(a) << endl;
+    // cout << ch << " Size of a is=" << sizeof(ch) << endl;
+    // cout << b1 << " Size of a is=" << sizeof(b1) << endl;
+    // cout << c << " Size of a is=" << sizeof(c) << endl;
+    // cout << d << " Size of a is=" << sizeof(d) << endl;
+
+    // TYPE CASTING
+    // int n;
+    // n = '@';
+    // cout << n << endl;
+    // char f = 98;
+    // cout << f << endl;
+
+    unsigned int a=123;//only positive number can be store in unsigned.
+    cout<<a<<endl;
+    unsigned int b=-23;//negative number in unsigned can not be store,
+    cout<<b<<endl;//it will give the position or garbage value
+
+
+    // opertators
+    // int a = 2;
+    // int b = 5;
+    // cout << a + b << endl;//7
+    // cout << a - b << endl;//-3
+    // cout << a * b << endl;//10
+    // cout << a / b << endl;//0 because jab integer ko integer se divide kroge toh integer hi aayega
+    // cout << a % b << endl;//2
+    // float c=a/b;
+    // cout<<c<<endl;//0 because jab integer ko integer se divide kroge toh integer hi aayega
+
+    // Relational operator
+    // int a = 5;
+    // int b = 3;
+    // cout << (a == b) << endl; // false ->0
+    // cout << (a >= b) << endl; // true ->1
+    // cout << (a <= b) << endl; // false ->0
+    // cout << (a > b) << endl;  // true ->1
+    // cout << (a < b) << endl;  // false ->0
+}
