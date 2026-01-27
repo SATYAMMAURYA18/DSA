55ddf70b95599ccfb590a524c15f5ef04b566531
SATYAM MAURYA <satyammaurya24363@gmail.com>
2026-01-27 07:15:47 +0530

    Pattern

--- day3/pattern/basic_pattern.cpp
+++ day3/pattern/basic_pattern.cpp
@@ -1,46 +1,230 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    //1.pattern
+    // 1.pattern
+    //  int i=1;
+    //  while(i<=3)
+    //  {
+    //      int j=1;
+    //      while (j<=3)
+    //      {
+    //          cout<<"*"<<" ";
+    //          j++;
+    //      }
+    //      cout<<endl;
+    //      i++;
+    //  }
+
+    // 2.pattern
+    //  int i=1;
+    //  while(i<=3)
+    //  {
+    //      int j=1;
+    //      while (j<=3)
+    //      {
+    //          cout<<j<<" ";
+    //          j++;
+    //      }
+    //      cout<<endl;
+    //      i++;
+    //  }
+
+    // 3.pattern
+    //  int i=1;
+    //  while(i<=3)
+    //  {
+    //      int j=1;
+    //      while (j<=3)
+    //      {
+    //          cout<<i<<" ";
+    //          j++;
+    //      }
+    //      cout<<endl;
+    //      i++;
+    //  }
+
+    // 4.trangle pattern
+    //  int i=1;
+    //  while(i<=3)
+    //  {
+    //      int j=1;
+    //      while (j<=i)
+    //      {
+    //          cout<<"*"<<" ";
+    //          j++;
+    //      }
+    //      cout<<endl;
+    //      i++;
+    //  }
+
+    // 5.
+    //   int i=1;
+    //  while(i<=3)
+    //  {
+    //      int j=1;
+    //      while (j<=i)
+    //      {
+    //          cout<<i<<" ";
+    //          j++;
+    //      }
+    //      cout<<endl;
+    //      i++;
+    //  }
+
+    // 6.
+    //   int i=1;
+    //   int count=1;
+    //  while(i<=3)
+    //  {
+    //      int j=1;
+    //      while (j<=i)
+    //      {
+    //          cout<<count<<" ";
+    //          count++;
+    //          j++;
+    //      }
+    //      cout<<endl;
+    //      i++;
+    //  }
+
+    //  int i=1;
+    // while(i<=4)
+    // {
+    //     int j=1;
+    //     int value=i;
+    //     while (j<=i)
+    //     {
+    //         cout<<value<<" ";
+    //         j++;
+    //         value++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+    // }
+
+    //   int i=1;
+    // while(i<=4)
+    // {
+    //     int j=1;
+    //     while (j<=i)
+    //     {
+    //         cout<<i-j+1<<" ";
+    //         j++;
+
+    //     }
+    //     cout<<endl;
+    //     i++;
+    // }
+    
     // int i=1;
     // while(i<=3)
     // {
+    //      int value=i;
     //     int j=1;
-    //     while (j<=3)
+    //     while (j<=i)
     //     {
-    //         cout<<"*"<<" ";
+    //         cout<<value<<" ";
+    //         value--;
     //         j++;
     //     }
     //     cout<<endl;
     //     i++;
     // }
 
-    //2.pattern
     // int i=1;
-    // while(i<=3)
+    // int ch=65;
+    // while(i<=4)
     // {
     //     int j=1;
-    //     while (j<=3)
+    //     int value=i;
+    //     while (j<=4)
     //     {
-    //         cout<<j<<" ";
+    //         cout<<char(ch)<<" ";
     //         j++;
+    //         value++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+    //     ch++;
+    // }
+
+    // int i=1;
+    // while(i<=4)
+    // {
+    //     int j=1;
+    //     int value=i;
+    //     while (j<=4)
+    //     {
+    //         char ch='A'+i-1;
+    //         cout<<ch<<" ";
+    //         j++;
+    //         value++;
     //     }
     //     cout<<endl;
     //     i++;
     // }
 
-    //3.pattern
-    // int i=1;
-    // while(i<=3)
+    //  int i=1;
+    // while(i<=4)
     // {
+
     //     int j=1;
-    //     while (j<=3)
+    //     int value=i;
+    //     while (j<=4)
     //     {
-    //         cout<<i<<" ";
+    //         char ch='A'+j-1;
+    //         cout<<ch<<" ";
     //         j++;
+    //         value++;
     //     }
     //     cout<<endl;
     //     i++;
     // }
+
+    //  int i=1;
+    // while(i<=4)
+    // {
+
+    //     int j=1;
+    //     int value=64+i;
+    //     while (j<=4)
+    //     {
+    //         char ch=value;
+    //         cout<<ch<<" ";
+    //         j++;
+    //         value++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+    // }
+
+    // int i = 1;
+    // while (i <= 4)
+    // {
+    //     int j = 1;
+    //     while (j < 4 - i + 1)
+    //     {
+    //         cout << " ";
+    //         j++;
+    //     }
+
+    //     int k = 1;
+    //     while (k <= i)
+    //     {
+    //         cout << k;
+    //         k++;
+    //     }
+
+    //     int l = 1;
+    //     while (l < i)
+    //     {
+    //         cout << i - l;
+    //         l++;
+    //     }
+
+    //     cout << endl;
+    //     i++;
+    // }
+
 }
